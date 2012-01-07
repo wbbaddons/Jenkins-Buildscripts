e2e19fcddf1f9480f0e5493ff42fa236d94940b6
RouL <roul@codingcorner.info>
2012-01-07 17:10:48 +0100

    Use wcf.zip artifact

--- eaBuild.sh
+++ eaBuild.sh
@@ -3,18 +3,27 @@
 [ -z $PACKAGE_NAME ] && exit 1
 [ -z $ZIP_NAME ] && exit 1
 [ ! -e "$PACKAGE_NAME.tar" ] && exit 1
+[ ! -e "build/wcf.zip" ] && exit 1
 
-test -e wcfsetup && cp -R wcfsetup/* build/wcfsetup/
-cp $PACKAGE_NAME.tar build/wcfsetup/install/packages/$PACKAGE_NAME.tar
+echo "\nPreparing WCF Setup structure"
+echo "-----------------------------"
+cd build
+mkdir wcfsetup
+unzip wcf.zip -d wcfsetup
+rm wcf.zip
+cd wcfsetup
+tar -xvzf WCFSetup.tar.gz
+test -e ../../wcfsetup && cp -R ../../wcfsetup/* ./
+cp ../../$PACKAGE_NAME.tar install/packages/$PACKAGE_NAME.tar
 
 echo "\nBuilding WCFSetup.tar.gz"
 echo "------------------------"
-cd build/wcfsetup
 tar cvzf WCFSetup.tar.gz install setup
-rm build/wcfsetup/install/packages/$PACKAGE_NAME.tar
 cp *.php ../../
 cp WCFSetup.tar.gz ../../
-cd ../..
+cd ..
+rm -rf wcfsetup
+cd ..
 
 echo "\nBuilding $ZIP_NAME.zip"
 echo "------------------------"
