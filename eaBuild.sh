c095e6ce54bf1b6da05d341ca3cc51a83056d93a
RouL <roul@codingcorner.info>
2012-01-07 16:29:07 +0100

    New build script 'system'

--- eaBuild.sh
+++ eaBuild.sh
@@ -1,33 +1,28 @@
 #!/bin/sh
 
-[ -z $1 ] && exit 1
-[ -z $2 ] && exit 1
-#[ ! -e "$1.tar" ] && exit 1
+[ -z $PACKAGE_NAME ] && exit 1
+[ -z $ZIP_NAME ] && exit 1
+[ ! -e "$PACKAGE_NAME.tar" ] && exit 1
 
-echo "Cleaning up\n-----------"
-test -e $2.zip && rm $2.zip
-test -e build/wcfsetup/WCFSetup.tar.gz && rm build/wcfsetup/WCFSetup.tar.gz
-test -e build/wcfsetup/instell/$1.tar && rm build/wcfsetup/instell/$1.tar
-
-cp $1.tar build/wcfsetup/install/packages/$1.tar
 test -e wcfsetup && cp -R wcfsetup/* build/wcfsetup/
+cp $PACKAGE_NAME.tar build/wcfsetup/install/packages/$PACKAGE_NAME.tar
 
 echo "\nBuilding WCFSetup.tar.gz"
 echo "------------------------"
 cd build/wcfsetup
 tar cvzf WCFSetup.tar.gz install setup
-test -e build/wcfsetup/install/packages/$1.tar && rm build/wcfsetup/install/packages/$1.tar
+rm build/wcfsetup/install/packages/$PACKAGE_NAME.tar
 cp *.php ../../
 cp WCFSetup.tar.gz ../../
 cd ../..
 
-echo "\nBuilding $2.zip"
+echo "\nBuilding $ZIP_NAME.zip"
 echo "------------------------"
 test -e COPYING && ADDITIONAL_FILES="$ADDITIONAL_FILES COPYING"
 test -e LICENSE && ADDITIONAL_FILES="$ADDITIONAL_FILES LICENSE"
 test -e README && ADDITIONAL_FILES="$ADDITIONAL_FILES README"
 test -e README.md && ADDITIONAL_FILES="$ADDITIONAL_FILES README.md"
-zip $2.zip WCFSetup.tar.gz *.php $ADDITIONAL_FILES
+zip $ZIP_NAME.zip WCFSetup.tar.gz *.php $ADDITIONAL_FILES
 rm *.php
 
 exit 0
--- packageBuild.sh
+++ packageBuild.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 
-[ -z $1 ] && exit 1
-echo "Cleaning up\n-----------"
-test -e acptemplates.tar && rm acptemplates.tar
-test -e files.tar && rm files.tar
-test -e templates.tar && rm templates.tar
-test -e $1.tar && rm $1.tar
-
+[ -z $PACKAGE_NAME ] && exit 1
 test -e acptemplates && echo "\nBuilding acptemplates.tar\n-------------------------" && cd acptemplates && tar cvf ../acptemplates.tar * && cd ..
 test -e files && echo "\nBuilding files.tar\n------------------" && cd files && tar cvf ../files.tar * && cd ..
 test -e templates && echo "\nBuilding templates.tar\n----------------------" && cd templates && tar cvf ../templates.tar * && cd ..
 
 
-echo "\nBuilding $1.tar"
+echo "\nBuilding $PACKAGE_NAME.tar"
 echo -n "-------------"
-for i in `seq 1 ${#1}`;
+for i in `seq 1 ${#PACKAGE_NAME}`;
 do
 		echo -n "-"
 done
 echo "\n"
-tar cvf $1.tar * --exclude acptemplates --exclude files --exclude templates --exclude WCFSetup.tar.gz
+tar cvf $PACKAGE_NAME.tar * --exclude acptemplates --exclude files --exclude templates
 
 test -e acptemplates.tar && rm acptemplates.tar
 test -e files.tar && rm files.tar
