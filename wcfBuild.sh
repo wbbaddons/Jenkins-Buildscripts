c3b1040fecf75e603d11c0f920e1ba1dea503a9a
RouL <roul@codingcorner.info>
2012-01-07 17:54:28 +0100

    Reenabled deletion of com.woltlab.wcf.tar in wcf-build

--- wcfBuild.sh
+++ wcfBuild.sh
@@ -9,4 +9,4 @@ tar -cf ../wcfsetup/install/packages/com.woltlab.wcf.tar * --exclude=template
 rm template.tar
 cd ../wcfsetup
 tar -czf ../WCFSetup.tar.gz install setup
-#rm install/packages/com.woltlab.wcf.tar
+rm install/packages/com.woltlab.wcf.tar
