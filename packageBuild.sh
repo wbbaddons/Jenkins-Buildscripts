259890fa5bf1fd7d058bbea6472b4491b1c66616
Tim Düsterhus <timwolla@googlemail.com>
2012-03-31 00:06:24 +0300

    File PIPs with plural are supported now as well.

--- packageBuild.sh
+++ packageBuild.sh
@@ -4,6 +4,9 @@
 test -e acptemplates && echo "\nBuilding acptemplates.tar\n-------------------------" && cd acptemplates && tar cvf ../acptemplates.tar * && cd ..
 test -e files && echo "\nBuilding files.tar\n------------------" && cd files && tar cvf ../files.tar * && cd ..
 test -e templates && echo "\nBuilding templates.tar\n----------------------" && cd templates && tar cvf ../templates.tar * && cd ..
+test -e acptemplate && echo "\nBuilding acptemplate.tar\n-------------------------" && cd acptemplate && tar cvf ../acptemplate.tar * && cd ..
+test -e file && echo "\nBuilding file.tar\n------------------" && cd file && tar cvf ../file.tar * && cd ..
+test -e template && echo "\nBuilding template.tar\n----------------------" && cd template && tar cvf ../template.tar * && cd ..
 
 
 echo "\nBuilding $PACKAGE_NAME.tar"
@@ -13,10 +16,14 @@ do
 		echo -n "-"
 done
 echo "\n"
-tar cvf $PACKAGE_NAME.tar * --exclude acptemplates --exclude files --exclude templates
+tar cvf $PACKAGE_NAME.tar * --exclude acptemplates --exclude files --exclude templates --exclude acptemplate --exclude file --exclude template
 
 test -e acptemplates.tar && rm acptemplates.tar
 test -e files.tar && rm files.tar
 test -e templates.tar && rm templates.tar
+test -e acptemplate.tar && rm acptemplate.tar
+test -e file.tar && rm file.tar
+test -e template.tar && rm template.tar
+
 
 exit 0
