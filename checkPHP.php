469f945c9b076a22dc9373a3dfd3992673aeac87
Tim Düsterhus <timwolla@arcor.de>
2012-04-17 21:19:59 +0200

    Adding file to check PHP for Syntax-Errors

---
+++ checkPHP.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+$check = null;
+$check = function ($folder) use (&$check) {
+	if (is_file($folder)) {
+		if (substr($folder, -4) === '.php') {
+			passthru('php -l '.escapeshellarg($folder), $code);
+			if ($code != 0) exit($code);
+		}
+		
+		return;
+	}
+	$files = glob($folder.'/*');
+	foreach ($files as $file) {
+		$check($file);
+	}
+};
+if (file_exists('file')) $check('file');
+if (file_exists('files')) $check('files');
