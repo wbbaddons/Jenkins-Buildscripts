b0a9169a4b825f3db4403b80cdf0fad4fe96cc8d
Tim Düsterhus <timwolla@googlemail.com>
2013-06-05 00:21:41 +0300

    Fix 6a7079158f075fa13977c75eda9bafc8cb11da32

--- wcfBuild.sh
+++ wcfBuild.sh
@@ -6,7 +6,7 @@ cd ./com.woltlab.wcf/templates/
 tar -cf ../templates.tar *
 cd ../
 tar -cf ../wcfsetup/install/packages/com.woltlab.wcf.tar * --exclude=template
-rm template.tar
+rm templates.tar
 cd ../wcfsetup
 tar -czf ../WCFSetup.tar.gz install setup
 rm install/packages/com.woltlab.wcf.tar
