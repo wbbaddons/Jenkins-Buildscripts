d4c526538ed8b031660f39eaf0331d3b6ee14d6c
Tim Düsterhus <timwolla@googlemail.com>
2013-06-05 00:13:53 +0300

    Adapt to WoltLab/WCF@57f801acdb3b452855418e4fa4e2aebaa70a5d84

--- wcfBuild.sh
+++ wcfBuild.sh
@@ -2,8 +2,8 @@
 
 # Building WCFSetup.tar.gz
 
-cd ./com.woltlab.wcf/template/
-tar -cf ../template.tar *
+cd ./com.woltlab.wcf/templates/
+tar -cf ../templates.tar *
 cd ../
 tar -cf ../wcfsetup/install/packages/com.woltlab.wcf.tar * --exclude=template
 rm template.tar
